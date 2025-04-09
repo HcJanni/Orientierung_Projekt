02004e2f693c63a42ebae9da02095f204acadc32
Wladimir Kloster <wladimir.kloster@hs-osnabrueck.de>
2025-04-09 11:26:32 +0200

    Basisklassen soweit erstellt. Um Animationen kümmere ich mich später :)

--- OrientierungProjekt/src/main/java/module-info.java
+++ OrientierungProjekt/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example.orientierungprojekt {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
 
     requires org.controlsfx.controls;
     requires com.almasb.fxgl.all;
